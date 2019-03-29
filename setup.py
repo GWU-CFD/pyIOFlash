a6521cccf0fc9d0bacb2406505bdd5b5e1f80c74
Aaron Lentner <aaron.d.lentner@gmail.com>
2019-03-29 15:14:00 -0400

    create setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyioflash',
+      version='0.17',
+      description='A Python Package for processing FLASH4 simulations',
+      url='https://github.com/Balaras-Group/pyIOFlash',
+      author='Aaron Lentner',
+      author_email='aaronlentner@gwmail.gwu.edu',
+      license='MIT',
+      packages=['pyioflash'],
+      zip_safe=False)
