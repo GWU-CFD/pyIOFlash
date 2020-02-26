4946a55c6b31bd53ab23c27fa9e4708f85f27d10
akashdhruv <akashdhruv@gwmail.gwu.edu>
2020-02-26 15:57:22 -0500

    setting up postprocessing branch

---
+++ pyioflash/postprocessing/pyio_bubbleprops.py
@@ -0,0 +1,11 @@
+import numpy
+import skimage.measure
+
+class SimulationAnalysis(object):
+
+    def __init__(self,data):
+
+        self.data = data
+
+    def get_bubbleprops():
+        self.bubbleprops # = function(self.data.fields['dfun'][tindex,blockindex,:,:,:])
