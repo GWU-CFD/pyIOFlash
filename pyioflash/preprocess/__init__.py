4228a4fcf33051e1848b1edc87a64a5e670bef15
Aaron Lentner <aaron.d.lentner@gmail.com>
2020-11-06 09:03:56 -0500

    first dev for chaining

---
+++ pyioflash/preprocess/__init__.py
@@ -0,0 +1,9 @@
+"""
+Python sub-package for providing methods to  
+pre-process simulation inputs and parameters; specifically, 
+methods to ...
+"""
+
+# Used to povide bootstraping in complexity  
+from pyioflash.preprocess import chaining
+
---
+++ pyioflash/preprocess/chaining/__init__.py
@@ -0,0 +1,7 @@
+"""
+Python sub-sub-package for providing methods to ... 
+"""
+
+# Used to .....
+from pyioflash.preprocess.chaining import domain
+
